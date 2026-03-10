6313ae320a1f9da6e9373f22fc59efbdb1c09900
mohantakumarganesh-alt <mohanta.kumarganesh@gmail.com>
2026-03-10 16:02:29 +0530

    Prepare for Render deployment

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--qcyx9i4!_n-1jl197%^vvy6&#t7_yf(#uxjt*js+cgn)_!-an
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'

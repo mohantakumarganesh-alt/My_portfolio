8787e35713187a71dcdee077015ed8a7d33a1a86
mohantakumarganesh-alt <mohanta.kumarganesh@gmail.com>
2026-03-19 12:08:42 +0530

    Configure secure session handling and deployment fixes for blog app

--- projects_code/blog_app/build.sh
+++ projects_code/blog_app/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install requirements
+pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --noinput
 python manage.py migrate
-# build.sh handles only build-time setups (collectstatic, migrate)
